6f9c3e8cb389122fe3ff69337e19a174b013dc0e
Anderson Rodrigo <andersonrodrigo03.14@gmail.com>
2021-07-07 19:02:38 -0300

    Adicionar projeto

---
+++ SignalRChatServer/SignalRChatServer/Hub/Chat.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Collections.Generic;
+
+namespace SignalRChatServer
+{
+    public class Chat : Hub
+    {
+        public static List<Message> Messages;
+
+        public Chat()
+        {
+            if (Messages == null)
+                Messages = new List<Message>();
+        }
+
+        public void NewMessage(string userName, string text)
+        {
+            Clients.All.SendAsync("newMessage", userName, text);
+            Messages.Add(new Message()
+            {
+                Text = text,
+                UserName = userName
+
+            });
+        }
+
+        public void NewUser(string userName, string connectionId)
+        {
+            Clients.Client(connectionId).SendAsync("previosMessages", Messages);
+            Clients.All.SendAsync("newUser", userName);
+        }
+    }
+
+    public class Message
+    {
+        public string UserName { get; set; }
+        public string Text { get; set; }
+    }
+}
